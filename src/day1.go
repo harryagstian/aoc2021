2de1e34eb3093ab9ee7444ec5696eab87e42c33d
Harry Agustian <harry@srlabs.id>
2021-12-01 12:18:29 +0700

    Day 1 solution

--- src/day1.go
+++ src/day1.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func day1() {
-	fmt.Printf("Solved day 1")
+	// lines, _ := readLines("day1sample.txt")
+	lines, _ := readLines("day1.txt")
+
+	current := 0
+	increase := 0
+	// part 1
+	for _, strValue := range lines {
+		new := stringToInteger(strValue)
+
+		if current == 0 {
+			current = new
+		}
+		if new > current {
+			increase = increase + 1
+		}
+		current = new
+	}
+	fmt.Printf("%d\n", increase)
+
+	// part 2
+	increase = 0
+	current = 0
+	for index, _ := range lines {
+		if index > len(lines)-3 {
+			break
+		}
+		new := 0
+		for i := 0; i < 3; i++ {
+			new = new + stringToInteger(lines[index+i])
+		}
+
+		if current == 0 {
+			current = new
+		}
+		if new > current {
+			increase = increase + 1
+		}
+		current = new
+	}
+	fmt.Printf("%d\n", increase)
 }
---
+++ src/utility.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func stringToInteger(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
