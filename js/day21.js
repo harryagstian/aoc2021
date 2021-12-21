2e9f919232ab24142faf1a4a350ff4967ed123d3
Harry Agustian <harryagustian777@yahoo.co.id>
2021-12-21 22:20:05 +0700

    day 21 part 2 draft

--- js/day21.js
+++ js/day21.js
@@ -1,51 +1,94 @@
 const { readFromFile, printSolution } = require('./utility')
 const flags = require('flags')
+const _ = require('lodash')
+
+const dp = {}
+
+let win = [0, 0]
 
 const solve = (sample) => {
     const inputs = readFromFile(21, sample).split("\n")
+    let basePlayerPosition = []
     let playerPosition = []
-    let playerScore = []
+    let playerScore = [0, 0]
     for (let line of inputs) {
         playerPosition.push(Number(line.split(": ").pop()))
-        playerScore.push(0)
     }
+
+    basePlayerPosition = _.cloneDeep(playerPosition)
 
     let done = false
     let currentDie = 1
     let diceRollCount = 0
-    while (!done) {
-        for (let i = 0; i < playerPosition.length; i++) {
-            let moves = (currentDie * 3) + 3
-            diceRollCount += 3
-            currentDie += 3
+    // while (!done) {
+    //     for (let i = 0; i < playerPosition.length; i++) {
+    //         let moves = (currentDie * 3) + 3
+    //         diceRollCount += 3
+    //         currentDie += 3
 
-            playerPosition[i] += moves
+    //         playerPosition[i] += moves
+    //         playerPosition[i] = calculatePosition(playerPosition[i])
 
-            if (playerPosition[i] % 10 === 0) {
-                playerPosition[i] = 10
-            } else {
-                playerPosition[i] %= 10
-            }
+    //         playerScore[i] += playerPosition[i]
 
-            if (playerPosition[i] === 0) {
-                playerPosition[i]++
-            }
+    //         if (playerScore[i] >= 1000) {
+    //             done = true
+    //             break
+    //         }
+    //     }
+    // }
 
+    // printSolution(playerScore.filter(e => e < 1000).shift() * diceRollCount)
 
-            playerScore[i] += playerPosition[i]
+    playerPosition = basePlayerPosition
+    playerScore = [0, 0]
 
-            if (playerScore[i] >= 1000) {
-                done = true
-                break
-            }
-        }
-    }
-
-    printSolution(playerScore.filter(e => e < 1000).shift() * diceRollCount)
+    play(playerPosition, playerScore)
+    console.log(win)
+    console.log(dp)
 }
 
-flags.defineBoolean("sample", false, "run with sample")
-flags.defineBoolean("s", false, "run with sample")
-flags.parse()
+const play = (playerPosition, playerScore) => {
+    console.log({playerPosition, playerScore})
+    let playerPositionCopy = _.cloneDeep(playerPosition)
+    let playerScoreCopy = _.cloneDeep(playerScore)
+    let key = createKey(playerPosition, playerScore)
+    if (dp[key]) {
+        win[dp[key]]++
+        return
+    }
 
-solve(flags.get("sample") || flags.get("s"))
+    for (let i = 0; i < 2; i++) {
+        if (playerScore[i] >= 21) {
+            console.log(playerScore[i])
+            win[i]++
+            dp[key] = i
+            win[i]++
+            return
+        }
+
+        for (let j = 0; j < 3; j++) {
+            playerPositionCopy[i] = calculatePosition(playerPositionCopy[i] + 1)
+            playerScoreCopy[i] = playerScore[i] + playerPositionCopy[i]
+            play(playerPositionCopy, playerScoreCopy)
+        }
+    }
+}
+
+const calculatePosition = (current) => {
+    if (current % 10 === 0) {
+        return 10
+    }
+
+    return current % 10
+}
+
+const createKey = (...rest) => rest.join(".")
+
+// flags.defineBoolean("sample", false, "run with sample")
+// flags.defineBoolean("s", false, "run with sample")
+// flags.parse()
+
+// solve(flags.get("sample") || flags.get("s"))
+// solve(true)
+solve(false)
